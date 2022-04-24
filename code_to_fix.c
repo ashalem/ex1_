070b48bc344dbb38e489ac9f9e918afee197fa07
ashalem <arielshalem1@gmail.com>
2022-04-24 18:30:07 +0300

    fix: Fixed code_to_fix.c (code_fixes)

--- code_to_fix.c
+++ code_to_fix.c
@@ -1,16 +1,23 @@
-#include "stdlib.h”
-#include "string.h”
-#include "assert.h”
+#include <stdlib.h> //changed
+#include <string.h> //changed
+#include <assert.h> //changed
 
-char* stringDuplicator(char* s, int times){
-    assert(!s);
+// s -> ptr_string
+char* stringDuplicator(char* ptr_str, int times){
+    assert(ptr_str); // Assert check it is null
     assert(times > 0);
-    int LEN = strlen(s);
-    char* out = malloc(LEN*times);
-    assert(out);
-    for (int i=0; i<=times; i++){
-    out = out + LEN;
-    strcpy(out,s);
+    int len = strlen(ptr_str); // Conventions LEN->len
+    char* ptr_out = malloc(len * times + 1); // Conventions + off by-one, maybe the sizeof
+    // Need to save out head
+    assert(ptr_out);
+
+    char* ptr_curr = ptr_out;
+    for (int i=0; i< times; i++){ // off by one in I
+        // Indentation
+        
+        strcpy(ptr_curr, ptr_str);
+        // Switched order
+        ptr_curr = ptr_curr + len;
     }
-    return out;
+    return ptr_out;
 }
